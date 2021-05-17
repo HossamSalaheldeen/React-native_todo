34bba5d61b3686bfea891fc2b73423648bd7cbd0
HossamSalaheldeen <hossam.salah1995@gmail.com>
2021-05-17 16:33:57 +0200

    first commit

---
+++ components/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+  
+  onLogin() {
+    const { email, password } = this.state;
+    const options = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    }
+    fetch('https://reqres.in/api/login', options)
+    .then(res => res.json())
+    .then(Alert.alert('successful login', `${email} + ${password}`))
+    // .catch(Alert.alert('fail login'));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.email}
+          onChangeText={(email) => this.setState({ email })}
+          placeholder={'Email'}
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+});
